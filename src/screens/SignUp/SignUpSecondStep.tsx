6cab0d6558693458e80af243e28862344278d0e2
luiizsilverio <luiiz.silverio@gmail.com>
2021-08-12 21:26:42 -0300

    SignUp com Yup

---
+++ src/screens/SignUp/SignUpSecondStep.tsx
@@ -0,0 +1,104 @@
+import React, { useState } from 'react'
+import { useNavigation, useRoute } from '@react-navigation/native'
+
+import { 
+  Alert,
+  Keyboard, 
+  KeyboardAvoidingView, 
+  TouchableWithoutFeedback 
+} from 'react-native'
+
+import { BackButton } from '../../components/BackButton'
+import { Bullet } from '../../components/Bullet'
+import { PasswordInput } from '../../components/PasswordInput'
+import { Button } from '../../components/Button'
+import { useTheme } from 'styled-components'
+
+import { 
+  Container, 
+  Header, 
+  BulletContainer,
+  Title,
+  SubTitle,
+  Form,
+  FormTitle  
+} from './styles'
+import theme from '../../styles/theme'
+
+interface Params {
+  user: {
+    name: string
+    email: string
+    cnh: string
+  }
+}
+
+export function SignUpSecondStep() {
+  const [password, setPassword] = useState('')
+  const [passwordConfirm, setPasswordConfirm] = useState('')
+  
+  const navigation = useNavigation()
+  const route = useRoute()
+
+  const { user } = route.params as Params
+
+  function handleRegister() {
+    if (!password || !passwordConfirm) {
+      return Alert.alert('Informe a senha e a confirmação')
+    }
+    if (password !== passwordConfirm) {
+      return Alert.alert('Senhas não conferem')
+    }
+
+    // Enviar para API e cadastrar
+
+    
+  }
+
+  return (
+    <KeyboardAvoidingView behavior="position" enabled>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      
+        <Container>
+          <Header>
+            <BackButton />
+            <BulletContainer>
+              <Bullet active />
+              <Bullet />
+              <Bullet />
+            </BulletContainer>
+          </Header>
+
+          <Title>Crie sua {'\n'} conta</Title>
+          <SubTitle>
+            Faça seu cadastro de {'\n'}
+            forma rápida e fácil
+          </SubTitle>
+
+          <Form>
+            <FormTitle>2. Senha</FormTitle>
+            <PasswordInput
+              iconName="lock"
+              placeholder="Senha"
+              value="password"
+              onChangeText={setPassword}
+            />
+            <PasswordInput
+              iconName="lock"
+              placeholder="Repetir Senha"      
+              value="passwordConfirm"
+              onChangeText={setPasswordConfirm}        
+            />              
+          </Form>
+
+          <Button 
+            title="Próximo"
+            color={theme.colors.success}
+            onPress={handleRegister}
+          />
+        </Container>
+
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
+  )
+}
