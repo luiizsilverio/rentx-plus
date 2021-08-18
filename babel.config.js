5eeb73af0500173ed756dac6b0d16bdab913f700
luiizsilverio <luiiz.silverio@gmail.com>
2021-08-17 21:28:01 -0300

    Watermelon

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,12 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin']
+    presets: [
+      'babel-preset-expo',
+      "module:metro-react-native-babel-preset"
+    ],
+    plugins: [
+      "@babel/plugin-proposal-decorators", { "legacy": true }
+    ]
   };
 };
